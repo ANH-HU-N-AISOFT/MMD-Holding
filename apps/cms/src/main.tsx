f52c5be15067c89e295fe4586634c17d3d3794c4
lemanh-tuong <letuong05092000@gmail.com>
2024-04-24 18:48:11 +0700

    update: thêm service worker cache assets

--- apps/cms/src/main.tsx
+++ apps/cms/src/main.tsx
@@ -7,6 +7,19 @@ import './styles/reset.css';
 import './tailwind.css';
 import '~/packages/common/I18n/i18n';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log('Service worker registration successful:', registration);
+      })
+      .catch(error => {
+        console.log('Service worker registration failed:', error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <I18nextProvider i18n={i18next}>
