01f55b11a6aff551bd7dac48b494e2a004aca544
lemanh-tuong <letuong05092000@gmail.com>
2024-05-01 17:48:38 +0700

    fix: fix build vercel

--- apps/cms/src/utils/functions/disableDatePicker.ts
+++ apps/cms/src/utils/functions/disableDatePicker.ts
@@ -2,21 +2,25 @@ import { DatePickerProps } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
 
 export const disablePast: DatePickerProps<Dayjs>['disabledDate'] = date => {
+  // @ts-ignore
   return date.isBefore(dayjs().startOf('day'));
 };
 
 export const disableFuture: DatePickerProps<Dayjs>['disabledDate'] = date => {
+  // @ts-ignore
   return date.isAfter(dayjs().startOf('day'));
 };
 
 export const disableAfterCheckpoint =
   (checkpoint: Dayjs): DatePickerProps<Dayjs>['disabledDate'] =>
   date => {
+    // @ts-ignore
     return date.isAfter(dayjs(checkpoint).startOf('day'));
   };
 
 export const disableBeforeCheckpoint =
   (checkpoint: Dayjs): DatePickerProps<Dayjs>['disabledDate'] =>
   date => {
+    // @ts-ignore
     return date.isBefore(dayjs(checkpoint).startOf('day'));
   };
